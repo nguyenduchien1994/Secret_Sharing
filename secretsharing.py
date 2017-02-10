2e9728538ffec90330573abab262cabf3eb1b3aa
Nguyen <heinous@vlsi34.bu.edu>
2017-02-10 18:36:37 -0500

    Added AMD Encryption

--- secretsharing.py
+++ secretsharing.py
@@ -1,15 +1,17 @@
 from __future__ import division
 import sys
 
-#sys.path.append("/ad/eng/users/h/e/heinous/Desktop/Research/Secret_Sharing/Finite-Field")
-sys.path.append("/home/heinous/Desktop/Research/SecretSharing/Secret_Sharing/Finite-Field")
-sys.path.append("/home/heinous/Desktop/Research/SecretSharing/Secret_Sharing/reedsolomon")
+sys.path.append("/ad/eng/users/h/e/heinous/Desktop/Research/Secret_Sharing/Finite-Field")
+sys.path.append("/ad/eng/users/h/e/heinous/Desktop/Research/Secret_Sharing/reedsolomon")
+#sys.path.append("/home/heinous/Desktop/Research/SecretSharing/Secret_Sharing/Finite-Field")
+#sys.path.append("/home/heinous/Desktop/Research/SecretSharing/Secret_Sharing/reedsolomon")
 
 import ffield
 import reedsolo
 import random
 
 test_cases = 10000
+R = random.randint(2,2**8-1)
 
 def truncate(code,f):
     to_return = []
@@ -31,7 +33,6 @@ def robustEncoder(S,f):
     int_S2 = int(bin_S2,2)
 
     int_E3 = F.Add(F.Multiply(int_S0,int_S1),F.Multiply(F.Multiply(int_S2,int_S2),int_S2))
-
     bin_E3 = '{0:08b}'.format(int_E3)
 
     return bin_S0 + bin_S1 + bin_S2 + bin_E3
@@ -58,6 +59,46 @@ def robustDecoder(E,f):
     else:
         return False
 
+def amdEncoder(S,f):
+
+    F = ffield.FField(f)
+    trun_S = truncate(S,f)
+
+    bin_S0 = trun_S[0]
+    bin_S1 = trun_S[1]
+    bin_S2 = trun_S[2]
+
+    int_S0 = int(bin_S0,2)
+    int_S1 = int(bin_S1,2)
+    int_S2 = int(bin_S2,2)
+
+    int_E3 = F.Add(F.Multiply(int_S0,R),F.Add(F.Multiply(F.Multiply(R,R),int_S1),F.Multiply(F.Multiply(F.Multiply(R,R),R),int_S2)))
+    bin_E3 = '{0:08b}'.format(int_E3)
+
+    return bin_S0 + bin_S1 + bin_S2 + bin_E3
+
+def amdDecoder(E,f):
+
+    F = ffield.FField(f)
+    trun_E = truncate(E,f)
+
+    bin_S0 = trun_E[0]
+    bin_S1 = trun_E[1]
+    bin_S2 = trun_E[2]
+    bin_E3 = trun_E[3]
+
+    int_S0 = int(bin_S0,2)
+    int_S1 = int(bin_S1,2)
+    int_S2 = int(bin_S2,2)
+    int_E3 = int(bin_E3,2)
+
+    eval_E3 = F.Add(F.Multiply(int_S0,R),F.Add(F.Multiply(F.Multiply(R,R),int_S1),F.Multiply(F.Multiply(F.Multiply(R,R),R),int_S2)))
+
+    if eval_E3 == int_E3:
+        return True
+    else:
+        return False
+
 def shareDistribute(E,f,n):
 
     F = ffield.FField(f)
@@ -69,7 +110,6 @@ def shareDistribute(E,f,n):
         D = F.Add(F.Add(F.Multiply(F.Multiply(x,x),int(E,2)),F.Multiply(a1,x)),a0)
         D = '{0:032b}'.format(D)
         shares['{0:032b}'.format(x)] = D
-
     return shares
 
 def secretRecon(holders,f):
@@ -80,9 +120,7 @@ def secretRecon(holders,f):
         denom = F.Add(int(holders.keys()[0],2),int(holders.keys()[1],2))
         nom = F.Add(int(holders.values()[0],2),int(holders.values()[1],2))
         E = F.Multiply(nom,F.Inverse(denom))
-
         return '{0:032b}'.format(E)
-
     elif len(holders) == 3:
         E = 0
         for x in holders.keys():
@@ -93,11 +131,8 @@ def secretRecon(holders,f):
                 if y != x:
                     denom = F.Multiply(denom,F.Add(int(x,2),int(y,2)))
             E = F.Add(E,F.Multiply(nom,F.Inverse(denom)))
-
         return '{0:032b}'.format(E)
-
     else:
-
         return 0
 
 def test_M1_M2(f):
@@ -121,6 +156,8 @@ def test_M1_M2(f):
 
 def test_robust():
 
+    print "****** Robust Encryption ******"
+
     int_S = random.randint(0,2**24-1)
     bin_S = '{0:024b}'.format(int_S)
 
@@ -128,11 +165,11 @@ def test_robust():
 
     E = robustEncoder(bin_S,8)
 
-    print "###### The encrypted-secret is " + E + " ######"
+    print "###### The encrypted secret is " + E + " ######"
 
     shares = shareDistribute(E,32,10)
-    #chosen = dict((key,value) for key, value in shares.iteritems() if key in ('{0:032b}'.format(1),'{0:032b}'.format(2),'{0:032b}'.format(3))) # Python 2.6
-    chosen = {x: shares[x] for x in ('{0:032b}'.format(4),'{0:032b}'.format(2),'{0:032b}'.format(3))} # Python 2.7
+    chosen = dict((key,value) for key, value in shares.iteritems() if key in ('{0:032b}'.format(1),'{0:032b}'.format(2),'{0:032b}'.format(3))) # Python 2.6
+    #chosen = {x: shares[x] for x in ('{0:032b}'.format(4),'{0:032b}'.format(2),'{0:032b}'.format(3))} # Python 2.7
     E_con = secretRecon(chosen,32)
 
     print "###### Reconstructed secret is " + E_con + " ######"
@@ -142,6 +179,31 @@ def test_robust():
     else:
         print "###### Incorrect! ######"
 
+def test_amd():
+
+    print "****** AMD Encryption ******"
+
+    int_S = random.randint(0,2**24-1)
+    bin_S = '{0:024b}'.format(int_S)
+
+    print "###### The secret is " + bin_S + " ######"
+
+    E = amdEncoder(bin_S,8)
+
+    print "###### The encrypted secret is " + E + " ######"
+
+    shares = shareDistribute(E,32,10)
+    chosen = dict((key,value) for key, value in shares.iteritems() if key in ('{0:032b}'.format(1),'{0:032b}'.format(2),'{0:032b}'.format(3))) # Python 2.6
+    #chosen = {x: shares[x] for x in ('{0:032b}'.format(4),'{0:032b}'.format(2),'{0:032b}'.format(3))} # Python 2.7
+    E_con = secretRecon(chosen,32)
+
+    print "###### Reconstructed secret is " + E_con + " ######"
+
+    if amdDecoder(E_con,8):
+        print "###### Correct! ######"
+    else:
+        print "###### Incorrect! ######"
+
 #test_M1_M2(8)
 test_robust()
-
+test_amd()
